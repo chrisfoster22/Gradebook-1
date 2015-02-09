fdcde08af30ebbe1b80074761e9b0b25e7207401
Chris Foster <fosterchris22@gmail.com>
2015-02-09 16:09:29 -0500

    Add achievements table.

---
+++ db/migrate/20150209210601_achievements.rb
@@ -0,0 +1,4 @@
+class Achievements < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209203314) do
+ActiveRecord::Schema.define(version: 20150209210601) do
+
+  create_table "achievements", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "points"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "parents", force: :cascade do |t|
     t.string   "name"
